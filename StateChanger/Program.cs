508d6b30b282c27e8be7239c15ab78244e88be2d
RoxasCodes <evicokazzz@gmail.com>
2021-12-06 18:54:39 +0200

    Add project files.

---
+++ StateChanger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkState
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
