08aa46857dede7f3b3040b7954d8b3052d7c5334
LiorAvitall <93089188+LiorAvitall@users.noreply.github.com>
2021-12-06 19:00:00 +0200

    Changes Lior

--- StateChanger/Program.cs
+++ StateChanger/Program.cs
@@ -1,15 +1,128 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWorkState
 {
-    class Program
+
+
+    public class PlayerController
     {
         static void Main(string[] args)
         {
+            PlayerController player = new PlayerController(new StandingState());
+            player.PressPlay();
+            player.StateChanger();
+            player.PressPlay();
+            player.PressPlay();
+            player.StateChanger();
+            player.PressPlay();
+            player.StateChanger();
+        }
+        private PlayerControllerState _state;
+
+        public PlayerController(PlayerControllerState state)
+        {
+            _state = state;
+        }
+
+        public void PressPlay()
+        {
+            _state.PressKey(this);
+        }
+
+        public void StateChanger()
+        {
+            _state.ChangeStance(this);
+        }
+
+        public PlayerControllerState CurrentState
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+    }
+
+
+    public abstract class PlayerControllerState
+    {
+        public abstract void PressKey(PlayerController player);
+
+        public abstract void ChangeStance(PlayerController player);
+    }
+
+
+    public class StandingState : PlayerControllerState
+    {
+        public StandingState()
+        {
+            Console.WriteLine("IM STANDING");
+        }
+
+        public override void PressKey(PlayerController player)
+        {
+            Console.WriteLine("UP Arrow was pressed");
+        }
+
+        public override void ChangeStance(PlayerController player)
+        {
+            player.CurrentState = new JumppingState();
+        }
+    }
+
+
+    public class JumppingState : PlayerControllerState
+    {
+        public JumppingState()
+        {
+            Console.WriteLine("IM JUMPING");
+        }
+
+        public override void PressKey(PlayerController player)
+        {
+            player.CurrentState = new DivingState();
+        }
+
+        public override void ChangeStance(PlayerController player)
+        {
+            player.CurrentState = new DuckingState();
+        }
+    }
+
+
+    public class DivingState : PlayerControllerState
+    {
+        public DivingState()
+        {
+            Console.WriteLine("IM DIVING");
+        }
+
+        public override void PressKey(PlayerController player)
+        {
+            player.CurrentState = new JumppingState();
+        }
+
+        public override void ChangeStance(PlayerController player)
+        {
+            player.CurrentState = new DuckingState();
+        }
+    }
+
+
+    public class DuckingState : PlayerControllerState
+    {
+        public DuckingState()
+        {
+            Console.WriteLine("IM DUCKING");
+        }
+
+        public override void PressKey(PlayerController player)
+        {
+            Console.WriteLine("Down Arrow was pressed");
+        }
+
+        public override void ChangeStance(PlayerController player)
+        {
+            player.CurrentState = new StandingState();
         }
     }
 }
+
